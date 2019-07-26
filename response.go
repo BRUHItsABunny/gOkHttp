885ec992555c6b0c19c7ebe340f58d8f786f5f9a
BUNNY <323okhttp@bruhiitsabunny.uu.ma>
2019-07-26 21:05:27 +0200

    * Added a downloading feature * Got rid of ioutil.ReadAll * Optimised reading response

--- response.go
+++ response.go
@@ -1,104 +1,199 @@
 package gokhttp
 
 import (
+	"bytes"
 	"encoding/json"
 	"encoding/xml"
 	"errors"
 	"github.com/anaskhan96/soup"
 	"github.com/beevik/etree"
 	"github.com/buger/jsonparser"
+	"io"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
 func (res *HttpResponse) Text() (string, error) {
+
+	var body []byte
+	var err error
+
 	if strings.Contains(res.Header.Get("Content-Type"), "plain/text") || strings.Contains(res.Header.Get("Content-Type"), "text/plain") {
-		body, err := ioutil.ReadAll(res.Body)
-		if checkError(err) {
-			err = res.Body.Close()
-			if checkError(err) {
-				return string(body), err
-			}
-		}
-		return "", err
+		body, err = res.Bytes()
+	} else {
+		err = errors.New("content-type is: " + res.Header.Get("Content-Type"))
 	}
-	return "", errors.New("Content-Type is: " + res.Header.Get("Content-Type"))
+
+	if err == nil {
+		err = res.Body.Close()
+	} else {
+		_ = res.Body.Close()
+	}
+
+	return string(body), err
 }
 
 func (res *HttpResponse) Bytes() ([]byte, error) {
-	body, err := ioutil.ReadAll(res.Body)
-	if checkError(err) {
+
+	var body []byte
+	var buffer *bytes.Buffer
+	var err error
+
+	if res.ContentLength == -1 {
+		buffer = bytes.NewBuffer(body)
+		_, err = io.Copy(buffer, res.Body)
+		body = buffer.Bytes()
+	} else {
+		body = make([]byte, res.ContentLength)
+		buffer = bytes.NewBuffer(body)
+		_, err = io.Copy(buffer, res.Body)
+	}
+
+	if err == nil {
 		err = res.Body.Close()
-		if checkError(err) {
-			return body, err
+	} else {
+		_ = res.Body.Close()
+	}
+
+	return body, err
+}
+
+func (res *HttpResponse) Discard() error {
+
+	var err error
+
+	_, err = io.Copy(ioutil.Discard, res.Body)
+
+	if err == nil {
+		err = res.Body.Close()
+	} else {
+		_ = res.Body.Close()
+	}
+
+	return err
+}
+
+func (res *HttpResponse) SaveToFile(filename string, permissions os.FileMode, overwrite, append bool) error {
+
+	var err error
+	var info os.FileInfo
+	var f *os.File
+
+	info, err = os.Stat(filename)
+	if os.IsNotExist(err) {
+		f, err = os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, permissions)
+		if err == nil {
+			_, err = io.Copy(f, res.Body)
+		}
+	} else {
+		if !info.IsDir() {
+			switch true {
+			case overwrite:
+				f, err = os.OpenFile(filename, os.O_WRONLY|os.O_TRUNC, permissions)
+			case append:
+				f, err = os.OpenFile(filename, os.O_WRONLY|os.O_APPEND, permissions)
+			default:
+				err = errors.New("no action performed")
+			}
+			if err == nil {
+				_, err = io.Copy(f, res.Body)
+			}
+		} else {
+			err = errors.New("can't treat folder as a file")
 		}
 	}
-	return nil, err
+
+	if err == nil {
+		err = res.Body.Close()
+	} else {
+		_ = res.Body.Close()
+	}
+	return err
 }
 
 func (res *HttpResponse) JSON() (*HttpJSONResponse, error) {
+
+	var body []byte
+	var err error
+
 	if strings.Contains(res.Header.Get("Content-Type"), "application/json") {
-		body, err := ioutil.ReadAll(res.Body)
-		if checkError(err) {
-			err = res.Body.Close()
-			if checkError(err) {
-				return &HttpJSONResponse{body}, err
-			}
-		}
-		return nil, err
+		body, err = res.Bytes()
+	} else {
+		err = errors.New("content-type is: " + res.Header.Get("Content-Type"))
 	}
-	return nil, errors.New("Content-Type is: " + res.Header.Get("Content-Type"))
+
+	if err == nil {
+		err = res.Body.Close()
+	} else {
+		_ = res.Body.Close()
+	}
+	return &HttpJSONResponse{body}, err
 }
 
 func (res *HttpResponse) XML() (*etree.Document, error) {
+
+	var err error
+	var doc *etree.Document
+
 	if strings.Contains(res.Header.Get("Content-Type"), "application/xml") {
-		body, err := ioutil.ReadAll(res.Body)
-		if checkError(err) {
-			err = res.Body.Close()
-			if checkError(err) {
-				doc := etree.NewDocument()
-				err = doc.ReadFromBytes(body)
-				if checkError(err) {
-					return doc, err
-				}
-			}
-		}
-		return nil, err
+		doc = etree.NewDocument()
+		_, err = doc.ReadFrom(res.Body)
+	} else {
+		err = errors.New("content-type is: " + res.Header.Get("Content-Type"))
 	}
-	return nil, errors.New("Content-Type is: " + res.Header.Get("Content-Type"))
+
+	if err == nil {
+		err = res.Body.Close()
+	} else {
+		_ = res.Body.Close()
+	}
+	return doc, err
 }
 
 func (res *HttpResponse) HTML() (*soup.Root, error) {
+
+	var err error
+	var htmlSoup soup.Root
+	var body []byte
+
 	if strings.Contains(res.Header.Get("Content-Type"), "plain/html") || strings.Contains(res.Header.Get("Content-Type"), "text/html") {
-		body, err := ioutil.ReadAll(res.Body)
+		body, err = res.Bytes()
 		if checkError(err) {
 			err = res.Body.Close()
 			if checkError(err) {
-				htmlSoup := soup.HTMLParse(string(body))
-				return &htmlSoup, err
+				htmlSoup = soup.HTMLParse(string(body))
 			}
 		}
-		return nil, err
+	} else {
+		err = errors.New("content-type is: " + res.Header.Get("Content-Type"))
 	}
-	return nil, errors.New("Content-Type is: " + res.Header.Get("Content-Type"))
+
+	if err == nil {
+		err = res.Body.Close()
+	} else {
+		_ = res.Body.Close()
+	}
+	return &htmlSoup, err
 }
 
 func (res *HttpResponse) Object(o interface{}) error {
-	body, err := ioutil.ReadAll(res.Body)
-	if checkError(err) {
+
+	var err error
+
+	switch true {
+	case strings.Contains(res.Header.Get("Content-Type"), "application/xml"):
+		err = xml.NewDecoder(res.Body).Decode(o)
+	case strings.Contains(res.Header.Get("Content-Type"), "application/json"):
+		err = json.NewDecoder(res.Body).Decode(o)
+	default:
+		err = errors.New("content-type not supported: " + res.Header.Get("Content-Type"))
+	}
+
+	if err == nil {
 		err = res.Body.Close()
-		if checkError(err) {
-			switch true {
-			case strings.Contains(res.Header.Get("Content-Type"), "application/xml"):
-				err = xml.Unmarshal(body, o)
-				return err
-			case strings.Contains(res.Header.Get("Content-Type"), "application/json"):
-				err = json.Unmarshal(body, o)
-				return err
-			default:
-				return errors.New("Content-Type not supported: " + res.Header.Get("Content-Type"))
-			}
-		}
+	} else {
+		_ = res.Body.Close()
 	}
 	return err
 }
