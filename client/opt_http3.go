5f9a293571b4561d847e60589ab1890d126cef8b
BRUHITsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2022-12-17 02:23:37 +0100

    Add http3 support

---
+++ client/opt_http3.go
@@ -0,0 +1,19 @@
+package client
+
+import (
+	"github.com/lucas-clemente/quic-go/http3"
+	"net/http"
+)
+
+type HTTP3Option struct {
+	Transport *http3.RoundTripper
+}
+
+func NewHTTP3Option(trans *http3.RoundTripper) *HTTP3Option {
+	return &HTTP3Option{Transport: trans}
+}
+
+func (o *HTTP3Option) Execute(client *http.Client) error {
+	client.Transport = o.Transport
+	return nil
+}
