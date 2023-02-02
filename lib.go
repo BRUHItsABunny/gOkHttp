c533292355acde1db3329ac37e75655ed63046d3
BRUHITsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2023-02-02 08:42:31 +0100

    assume the env file is already loaded

--- lib.go
+++ lib.go
@@ -2,7 +2,6 @@ package gokhttp
 
 import (
 	"github.com/BRUHItsABunny/gOkHttp/client"
-	"github.com/joho/godotenv"
 	"net/http"
 	"os"
 )
@@ -11,19 +10,14 @@ func NewHTTPClient(options ...client.Option) (*http.Client, error) {
 	return client.NewHTTPClient(options...)
 }
 
-// TestHTTPClient is a function i end up using a lot in test files to control how my http.Client is initialized depending on the .env file in my project
+// TestHTTPClient is a function i end up using a lot in test files to control how my http.Client is initialized depending on the ENV variables USE_PROXY and PROXY_URL
 func TestHTTPClient() (*http.Client, error) {
 	hClient := http.DefaultClient
-	err := godotenv.Load(".env")
-	if err != nil {
-		return hClient, err
-	}
-
 	opts := []client.Option{}
 	if os.Getenv("USE_PROXY") == "true" {
 		opts = append(opts, client.NewProxyOption(os.Getenv("PROXY_URL")))
 	}
 
-	hClient, err = client.NewHTTPClient(opts...)
+	hClient, err := client.NewHTTPClient(opts...)
 	return hClient, err
 }
