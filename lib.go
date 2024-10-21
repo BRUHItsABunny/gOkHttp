e9fd5d358df19639395a23f8af14a084eb3cfe61
BRUHItsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2024-10-21 15:50:52 -0500

    feat: mtls to test client

--- lib.go
+++ lib.go
@@ -1,6 +1,9 @@
 package gokhttp
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
 	"github.com/BRUHItsABunny/gOkHttp/client"
 	"net/http"
 	"os"
@@ -16,6 +19,18 @@ func TestHTTPClient(options ...gokhttp_client.Option) (*http.Client, error) {
 	opts := []gokhttp_client.Option{}
 	if os.Getenv("USE_PROXY") == "true" {
 		opts = append(opts, gokhttp_client.NewProxyOption(os.Getenv("PROXY_URL")))
+	}
+	if os.Getenv("USE_MTLS") == "true" {
+		opt := gokhttp_client.NewMTLSOption(x509.NewCertPool(), []tls.Certificate{})
+		err := opt.AddCAFromFile(os.Getenv("CA_CERT"))
+		if err != nil {
+			return nil, fmt.Errorf("opt.AddCAFromFile: %w", err)
+		}
+		err = opt.AddClientCertFromFile(os.Getenv("CLIENT_CERT"), os.Getenv("CLIENT_KEY"))
+		if err != nil {
+			return nil, fmt.Errorf("opt.AddClientCertFromFile: %w", err)
+		}
+		opts = append(opts, opt)
 	}
 
 	opts = append(opts, options...)
