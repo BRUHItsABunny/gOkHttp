40a046d3caef599eceafecb4751422024657de36
BRUHItsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2024-11-09 17:32:15 -0500

    feat: add ability to run options before the test options from env

--- lib.go
+++ lib.go
@@ -13,6 +13,37 @@ func NewHTTPClient(options ...gokhttp_client.Option) (*http.Client, error) {
 	return gokhttp_client.NewHTTPClient(options...)
 }
 
+// NewTestHTTPClient is a function i end up using a lot in test files to control how my http.Client is initialized depending on the ENV variables USE_PROXY and PROXY_URL
+func NewTestHTTPClient(baseOpts []gokhttp_client.Option, options ...gokhttp_client.Option) (*http.Client, error) {
+	if os.Getenv("USE_PROXY") == "true" {
+		baseOpts = append(baseOpts, gokhttp_client.NewProxyOption(os.Getenv("PROXY_URL")))
+	}
+	if os.Getenv("USE_MTLS") == "true" {
+		opt := gokhttp_client.NewMTLSOption(x509.NewCertPool(), []tls.Certificate{})
+		err := opt.AddCAFromFile(os.Getenv("CA_CERT"))
+		if err != nil {
+			return nil, fmt.Errorf("opt.AddCAFromFile: %w", err)
+		}
+		err = opt.AddClientCertFromFile(os.Getenv("CLIENT_CERT"), os.Getenv("CLIENT_KEY"))
+		if err != nil {
+			return nil, fmt.Errorf("opt.AddClientCertFromFile: %w", err)
+		}
+		baseOpts = append(baseOpts, opt)
+	}
+	if os.Getenv("USE_WIRESHARK") == "true" {
+		opt, err := gokhttp_client.NewTLSKeyLoggingOptionToFile(os.Getenv("WIRESHARK_LOGFILE"))
+		if err != nil {
+			return nil, fmt.Errorf("gokhttp_client.NewTLSKeyLoggingOptionToFile: %w", err)
+		}
+		baseOpts = append(baseOpts, opt)
+	}
+
+	baseOpts = append(baseOpts, options...)
+
+	hClient, err := gokhttp_client.NewHTTPClient(baseOpts...)
+	return hClient, err
+}
+
 // TestHTTPClient is a function i end up using a lot in test files to control how my http.Client is initialized depending on the ENV variables USE_PROXY and PROXY_URL
 func TestHTTPClient(options ...gokhttp_client.Option) (*http.Client, error) {
 	hClient := http.DefaultClient
